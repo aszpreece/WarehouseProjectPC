0db78b8bbae6c4caa0a1a1efc33bca311dc76cb7
TJR735 <tjr735@student.bham.ac.uk>
2018-03-06 14:47:59 +0000

    small progress

--- src/GridMapToMesh.java
+++ src/GridMapToMesh.java
@@ -5,7 +5,7 @@ import rp.robotics.mapping.GridMap;
 import rp.robotics.mapping.MapUtils;
 
 public class GridMapToMesh {
-	
+
 	private final int GRID_WIDTH = 12;
 	private final int GRID_HEIGHT = 8;
 
@@ -16,32 +16,48 @@ public class GridMapToMesh {
 		map = MapUtils.createRealWarehouse();
 		createCompleteMesh(0);
 	}
-	
-	
-	
-	
-	public void createCompleteMesh(int ypos) {
-		//iterate across a row of the grid
+
+	private void createCompleteMesh(int ypos) {
+		// iterate across a row of the grid
 		for (int xpos = 0; xpos < GRID_WIDTH; xpos++) {
-			if (map.isValidGridPosition(xpos, ypos)) {//check that it is a valid grid position
-				if (!map.isObstructed(xpos, ypos)) {//check grid point isn't obstructed
-					Point coord = map.getCoordinatesOfGridPosition(xpos, ypos);//get the coordinates of the point
-					Node node = new Node (coord.x, coord.y);//create the node for the point
-					
-					//check how many neighbours the point will have
-					int noOfNeighbours = 0;//the number of neighbours
-					
-					
-					mesh.addNode(node, noOfNeighbours);
+			if (checkPointValidity(xpos, ypos)) {// check the point is valid
+				Point coord = map.getCoordinatesOfGridPosition(xpos, ypos);// get the coordinates of the point
+				Node node = new Node(coord.x, coord.y);// create the node for the point
+
+				// check how many neighbours the point will - check points around are valid
+				int noOfNeighbours = 0;// the number of neighbours
+				if (checkPointValidity(xpos - 1, ypos)) {
+					noOfNeighbours++;
 				}
-				
+				if (checkPointValidity(xpos + 1, ypos)) {
+					noOfNeighbours++;
+				}
+				if (checkPointValidity(xpos, ypos - 1)) {
+					noOfNeighbours++;
+				}
+				if (checkPointValidity(xpos, ypos + 1)) {
+					noOfNeighbours++;
+				}
+
+				mesh.addNode(node, noOfNeighbours);
 			}
+		}
+
+	}
+
+	private boolean checkPointValidity(int xpos, int ypos) {
+		if (map.isValidGridPosition(xpos, ypos)) {// check that it is a valid grid position
+			if (!map.isObstructed(xpos, ypos)) {// check grid point isn't obstructed
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
 		}
 	}
 }
 
-
-
-
-// iterate through gridmap checking if the node is valid, and if the nodes around it are valid
+// iterate through gridmap checking if the node is valid, and if the nodes
+// around it are valid
 // if it is, add to the mesh with the number of neighbours
