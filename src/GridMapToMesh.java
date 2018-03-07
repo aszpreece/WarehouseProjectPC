c58d5d90ce9056f857ee0b104adb10b726e8b9a5
TJR735 <tjr735@student.bham.ac.uk>
2018-03-07 12:38:32 +0000

    changing approach

--- src/GridMapToMesh.java
+++ src/GridMapToMesh.java
@@ -1,76 +1,18 @@
-import lejos.geom.Point;
-import lejos.robotics.pathfinding.NavigationMesh;
-import lejos.robotics.pathfinding.Node;
+import lejos.robotics.pathfinding.FourWayGridMesh;
 import rp.robotics.mapping.GridMap;
 import rp.robotics.mapping.MapUtils;
 
 public class GridMapToMesh {
-
-	private final int GRID_WIDTH = 12;
-	private final int GRID_HEIGHT = 8;
-
-	private NavigationMesh mesh;
+	
+	private FourWayGridMesh mesh;
 	private GridMap map;
 
 	public GridMapToMesh() {
 		map = MapUtils.createRealWarehouse();
-		createCompleteMesh(0);
+		mesh = new FourWayGridMesh(map, 0.3f, 0.17f);
 	}
-
-	public NavigationMesh createCompleteMesh(int ypos) {
-		// iterate across a row of the grid
-		for (int xpos = 0; xpos < GRID_WIDTH; xpos++) {
-			if (checkPointValidity(xpos, ypos)) {// check the point is valid
-				Point coord = map.getCoordinatesOfGridPosition(xpos, ypos);// get the coordinates of the point
-				Node node = new Node(coord.x, coord.y);// create the node for the point
-
-				// check how many neighbours the point will - check points around are valid
-				// if the neighbours are valid, connect them
-				int noOfNeighbours = 0;// the number of neighbours
-				if (checkPointValidity(xpos - 1, ypos)) {
-					Point leftNeighbour = map.getCoordinatesOfGridPosition(xpos - 1, ypos);
-					Node leftNode = new Node(leftNeighbour.x, leftNeighbour.y);
-					mesh.connect(node, leftNode);
-					noOfNeighbours++;
-				}
-				if (checkPointValidity(xpos + 1, ypos)) {
-					Point rightNeighbour = map.getCoordinatesOfGridPosition(xpos + 1, ypos);
-					Node rightNode = new Node(rightNeighbour.x, rightNeighbour.y);
-					mesh.connect(node, rightNode);
-					noOfNeighbours++;
-				}
-				if (checkPointValidity(xpos, ypos - 1)) {
-					Point downNeighbour = map.getCoordinatesOfGridPosition(xpos, ypos - 1);
-					Node downNode = new Node(downNeighbour.x, downNeighbour.y);
-					mesh.connect(node, downNode);
-					noOfNeighbours++;
-				}
-				if (checkPointValidity(xpos, ypos + 1)) {
-					Point upNeighbour = map.getCoordinatesOfGridPosition(xpos, ypos + 1);
-					Node upNode = new Node(upNeighbour.x, upNeighbour.y);
-					mesh.connect(node, upNode);
-					noOfNeighbours++;
-				}
-				mesh.addNode(node, noOfNeighbours);
-			}
-		}
-		// when it reaches the end of the row, call on next y value
-		ypos++;
-		if (ypos > GRID_HEIGHT == false) {
-			createCompleteMesh(ypos);
-		}
+	
+	public FourWayGridMesh createCompleteMesh() {
 		return mesh;
-	}
-
-	private boolean checkPointValidity(int xpos, int ypos) {
-		if (map.isValidGridPosition(xpos, ypos)) {// check that it is a valid grid position
-			if (!map.isObstructed(xpos, ypos)) {// check grid point isn't obstructed
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			return false;
-		}
 	}
 }
--- src/Pathfinding.java
+++ src/Pathfinding.java
@@ -1,27 +1,48 @@
+import lejos.robotics.navigation.DestinationUnreachableException;
 import lejos.robotics.navigation.Pose;
+import lejos.robotics.navigation.Waypoint;
 import lejos.robotics.pathfinding.AstarSearchAlgorithm;
-import lejos.robotics.pathfinding.NavigationMesh;
+import lejos.robotics.pathfinding.FourWayGridMesh;
+import lejos.robotics.pathfinding.Node;
 import lejos.robotics.pathfinding.NodePathFinder;
 
 public class Pathfinding {
-	
-	
+
 	Pose startPos;
+	Node startNode;
+	Waypoint goalPoint;
+	Node endNode;
 	NodePathFinder pathfinder;
 	AstarSearchAlgorithm aStar;
-	NavigationMesh mesh;
+	FourWayGridMesh mesh;
 	GridMapToMesh gridToMesh;
-	
+
 	public Pathfinding() {
 		gridToMesh = new GridMapToMesh();
-		mesh = gridToMesh.createCompleteMesh(0);
+		mesh = gridToMesh.createCompleteMesh();
 		startPos = new Pose();
+		goalPoint = new Waypoint(0, 0);
 		pathfinder = new NodePathFinder(aStar, mesh);
-		
-		System.out.println(mesh.getMesh());
+		setStartPoint(0, 0);
+		pathFind();
 	}
-	
+
+	private void setStartPoint(float x, float y) {
+		startPos.setLocation(x, y);
+		startNode = new Node(x, y);
+	}
+
+	private void setGoalPoint(float d, float y) {
+		goalPoint.setLocation(d, y);
+		endNode = new Node(d, y);
+	}
+
+	private void pathFind() {
+		setGoalPoint(2.44f, 0);
+		aStar.findPath(startNode, endNode);
+	}
+
 	public static void main(String[] args) {
-		
+		Pathfinding pathfind = new Pathfinding();
 	}
 }
