5cd32dc6d12643bce8b7e250da3c87932a536e10
tjr735 <tjr735@cs.bham.ac.uk>
2018-03-07 00:45:52 +0000

    added connecting nodes

--- src/GridMapToMesh.java
+++ src/GridMapToMesh.java
@@ -25,20 +25,32 @@ public class GridMapToMesh {
 				Node node = new Node(coord.x, coord.y);// create the node for the point
 
 				// check how many neighbours the point will - check points around are valid
+				//if the neighbours are valid, connect them
 				int noOfNeighbours = 0;// the number of neighbours
 				if (checkPointValidity(xpos - 1, ypos)) {
+					Point leftNeighbour = map.getCoordinatesOfGridPosition(xpos - 1, ypos);
+					Node leftNode = new Node(leftNeighbour.x, leftNeighbour.y);
+					mesh.connect(node, leftNode);
 					noOfNeighbours++;
 				}
 				if (checkPointValidity(xpos + 1, ypos)) {
+					Point rightNeighbour = map.getCoordinatesOfGridPosition(xpos + 1, ypos);
+					Node rightNode = new Node(rightNeighbour.x, rightNeighbour.y);
+					mesh.connect(node, rightNode);
 					noOfNeighbours++;
 				}
 				if (checkPointValidity(xpos, ypos - 1)) {
+					Point downNeighbour = map.getCoordinatesOfGridPosition(xpos, ypos - 1);
+					Node downNode = new Node(downNeighbour.x, downNeighbour.y);
+					mesh.connect(node, downNode);
 					noOfNeighbours++;
 				}
 				if (checkPointValidity(xpos, ypos + 1)) {
+					Point upNeighbour = map.getCoordinatesOfGridPosition(xpos, ypos + 1);
+					Node upNode = new Node(upNeighbour.x, upNeighbour.y);
+					mesh.connect(node, upNode);
 					noOfNeighbours++;
 				}
-
 				mesh.addNode(node, noOfNeighbours);
 			}
 		}
