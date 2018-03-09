017423631e537f05de94730b23f29f404a4cf57b
tcc746 <tcc746@cs.bham.ac.uk>
2018-03-09 11:32:08 +0000

    added addtional test case

--- WarehousePC/src/Files/test/JobAssignmentTests.java
+++ WarehousePC/src/Files/test/JobAssignmentTests.java
@@ -23,28 +23,69 @@ public class JobAssignmentTests {
 	public void simpleOneTaskTest() {
 		Item item = new Item(1, 1, 1.0f, 1.0f);
 		Task task = new Task("aa", 1, item);
-		ArrayList<Task> tasks = new ArrayList<Task>() {{add(task);}};
+		ArrayList<Task> tasks = new ArrayList<Task>() {
+			{
+				add(task);
+			}
+		};
+
+		// what the plan should be:
 		ArrayList<String> percievedPlan = new ArrayList<String>();
 		percievedPlan.add("aa1");
 		percievedPlan.add("drop");
 		ItemTable itemTable;
 		try {
 			itemTable = new ItemTable();
-			
+
 			RobotPC robot = new RobotPC();
 			robot.setCurrentWeight(0f);
 			robot.setCurrentX(0);
 			robot.setCurrentY(0);
-			
+
 			JobAssignment jobAllocator = new JobAssignment(itemTable);
-			ArrayList<String >actualPlan = jobAllocator.getNextPlan(tasks, robot);
+			ArrayList<String> actualPlan = jobAllocator.getNextPlan(tasks, robot);
 			Assert.assertArrayEquals(percievedPlan.toArray(), actualPlan.toArray());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 	}
-	
+
+	@Test
+	public void TwoTaskTest() {
+		Item item1 = new Item(1, 1, 1.0f, 1.0f);
+		Item item2 = new Item(2, 2, 1.0f, 1.0f);
+		Task task1 = new Task("aa", 1, item1);
+		Task task2 = new Task("ab", 1, item2);
+		ArrayList<Task> tasks = new ArrayList<Task>() {
+			{
+				add(task1);
+				add(task2);
+			}
+		};
+
+		// what the plan should be:
+		ArrayList<String> percievedPlan = new ArrayList<String>();
+		percievedPlan.add("ab1");
+		percievedPlan.add("aa1");
+		percievedPlan.add("drop");
+		ItemTable itemTable;
+		try {
+			itemTable = new ItemTable();
+
+			RobotPC robot = new RobotPC();
+			robot.setCurrentWeight(0f);
+			robot.setCurrentX(3);
+			robot.setCurrentY(3);
+
+			JobAssignment jobAllocator = new JobAssignment(itemTable);
+			ArrayList<String> actualPlan = jobAllocator.getNextPlan(tasks, robot);
+			Assert.assertArrayEquals(percievedPlan.toArray(), actualPlan.toArray());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
