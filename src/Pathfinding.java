1b3427658fbd3330624f3c83d5fb2b48536e4f6d
TJR735 <tjr735@student.bham.ac.uk>
2018-03-09 17:31:29 +0000

    currently getting unreachable error when using mesh

--- src/Pathfinding.java
+++ src/Pathfinding.java
@@ -1,3 +1,6 @@
+import lejos.robotics.navigation.DestinationUnreachableException;
+import lejos.robotics.navigation.Pose;
+import lejos.robotics.navigation.Waypoint;
 import lejos.robotics.pathfinding.AstarSearchAlgorithm;
 import lejos.robotics.pathfinding.FourWayGridMesh;
 import lejos.robotics.pathfinding.Node;
@@ -6,8 +9,8 @@ import lejos.robotics.pathfinding.Path;
 
 public class Pathfinding {
 
-	Node startNode;
-	Node endNode;
+	Pose startPose;
+	Waypoint endNode;
 	NodePathFinder pathfinder;
 	AstarSearchAlgorithm aStar;
 	FourWayGridMesh mesh;
@@ -16,22 +19,32 @@ public class Pathfinding {
 	public Pathfinding() {
 		gridToMesh = new GridMapToMesh();
 		mesh = gridToMesh.createCompleteMesh();
+		aStar = new AstarSearchAlgorithm();
 		pathfinder = new NodePathFinder(aStar, mesh);
-		setStartNode(0, 0);
+		startPose = new Pose(0.17f, 0.155f, 0);
+		endNode = new Waypoint(0, 0);
 		pathFind();
 	}
 
-	private void setStartNode(float x, float y) {
-		startNode = new Node(x, y);
-	}
+//	private void setStartNode(float x, float y) {
+//		startNode = new Node(x, y);
+//	}
 
-	private void setGoalNode(float d, float y) {
-		endNode = new Node(d, y);
+	private void setGoalWaypoint(float d, float y) {
+		endNode.setLocation(d, y);
 	}
 
 	private Path pathFind() {
-		setGoalNode(2.44f, 0);
-		return aStar.findPath(startNode, endNode);
+		setGoalWaypoint(2.1f, 3.36f);
+		Path path = new Path();
+		try {
+			path = pathfinder.findRoute(startPose, endNode);
+		} catch (DestinationUnreachableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(path.toString());
+		return path;
 	}
 
 	public static void main(String[] args) {
