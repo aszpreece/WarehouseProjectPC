f63a2b29b628c15822b476b4ccd7d4c55bdc583b
cxc880 <cxc880@student.bham.ac.uk>
2018-03-20 08:32:04 +0000

    multi path finding, have to test

--- WarehousePC/src/com/whpc/pathfinding/PathfindOnGraph.java
+++ WarehousePC/src/com/whpc/pathfinding/PathfindOnGraph.java
@@ -6,24 +6,30 @@ public class PathfindOnGraph {
 	
     final static int NODE_TRIED = 2;
     final static int PATH_NODE = 3;
-
+   
     public static int[][] graph = { 
     		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
-            {1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
-            {1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
-            {1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
-            {1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
+            {1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
+            {1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
+            {1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
+            {1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1},
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
     };
 
     private ArrayList<Byte> pathToFollow;
-
+    
+    public static ArrayList<Byte> Pathfinder() {
+    	ArrayList<Byte> multiList = new ArrayList<Byte>();
+    	
+    }
+    
     public static void main(String[] args) {
         PathfindOnGraph pathfinder = new PathfindOnGraph(graph);
         ShortestPathFinder shortest = new ShortestPathFinder(graph);
-
+        MultiPathfinder multiple = new MultiPathfinder(graph);
+        
         shortest.pathfind(2, 5, 0, 3);
         
   
