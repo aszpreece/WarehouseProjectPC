d555e71d5dc87758418d0c9d65c88af6e2fe5b7e
ocg785 <ocg785@cs.bham.ac.uk>
2018-03-22 11:34:01 +0000

    Completed Robot Tracking

--- WarehousePC/src/ui/PCGUI.java
+++ WarehousePC/src/ui/PCGUI.java
@@ -17,7 +17,10 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.SwingConstants;
@@ -26,8 +29,13 @@ import bluetooth.Robot;
 import filehandling.JobTable;
 import jobmanagement.Server;
 import lejos.util.Delay;
+import rp.robotics.LocalisedRangeScanner;
 import rp.robotics.MobileRobotWrapper;
+import rp.robotics.localisation.ActionModel;
 import rp.robotics.localisation.GridPositionDistribution;
+import rp.robotics.localisation.PerfectActionModel;
+import rp.robotics.localisation.PerfectSensorModel;
+import rp.robotics.localisation.SensorModel;
 import rp.robotics.mapping.GridMap;
 import rp.robotics.mapping.MapUtils;
 import rp.robotics.navigation.GridPilot;
@@ -65,11 +73,12 @@ public class PCGUI extends JFrame implements Runnable {
 	private JPanel inactiveJobsInnerPanel;
 
 	private GridPanel gridPanel;
-	private JPanel gridInnerPanel;
+	private JPanel robotDetailsPanel;
+	private RobotPanel robotDetailsInnerPanel;
 	private JMenuBar menuBar;
 	private JMenu toolsMenu;
 	private JMenuItem addRobotMenuItem;
-	
+
 	private int direction = -1;
 
 	private Server server;
@@ -90,7 +99,7 @@ public class PCGUI extends JFrame implements Runnable {
 		setPreferredSize(new Dimension(800, 600));
 		setTitle(FRAME_TITLE);
 		setVisible(true);
-				
+
 		jobsPanel = new JPanel(new FlowLayout());
 		jobsPanel.setPreferredSize(new Dimension(250, 500));
 
@@ -127,15 +136,26 @@ public class PCGUI extends JFrame implements Runnable {
 
 		gridPanel = new GridPanel(server);
 		{
-			gridPanel.setBorder(BorderFactory.createTitledBorder("Robots"));
+			gridPanel.setBorder(BorderFactory.createTitledBorder("Robot Warehouse"));
 			gridPanel.setPreferredSize(new Dimension(245, 500));
 			gridPanel.setLayout(new BoxLayout(gridPanel, BoxLayout.Y_AXIS));
-
-			gridInnerPanel = new JPanel();
-			gridInnerPanel.setLayout(new BoxLayout(gridInnerPanel, BoxLayout.X_AXIS));
 		}
 
+		Thread gridPanelThread = new Thread(gridPanel);
+
+		gridPanelThread.start();
+
 		add(gridPanel, BorderLayout.CENTER);
+		
+		robotDetailsPanel = new JPanel();
+//		{
+//			robotDetailsInnerPanel = new RobotPanel(server);
+//			robotDetailsPanel.setBorder(BorderFactory.createTitledBorder("Robots"));
+//			robotDetailsPanel.setPreferredSize(new Dimension(245, 100));
+//			robotDetailsPanel.setLayout(new BoxLayout(robotDetailsPanel, BoxLayout.X_AXIS));
+//		}
+
+		add(robotDetailsPanel, BorderLayout.SOUTH);
 		
 		menuBar = new JMenuBar();
 		toolsMenu = new JMenu("Tools");
@@ -153,12 +173,12 @@ public class PCGUI extends JFrame implements Runnable {
 				JLabel yLabel = new JLabel("Y: ");
 				JLabel dirLabel = new JLabel("Direction");
 				JButton okButton = new JButton("OK");
-				
+
 				JTextField xTextField = new JTextField();
-				xTextField.setPreferredSize(new Dimension(25,20));
+				xTextField.setPreferredSize(new Dimension(25, 20));
 				JTextField yTextField = new JTextField();
-				yTextField.setPreferredSize(new Dimension(25,20));
-				
+				yTextField.setPreferredSize(new Dimension(25, 20));
+
 				JRadioButton northButton = new JRadioButton("N");
 				northButton.addActionListener(new ActionListener() {
 
@@ -166,63 +186,63 @@ public class PCGUI extends JFrame implements Runnable {
 					public void actionPerformed(ActionEvent e) {
 						direction = 0;
 					}
-					
+
 				});
-				
+
 				JRadioButton southButton = new JRadioButton("S");
 				southButton.addActionListener(new ActionListener() {
 
 					@Override
 					public void actionPerformed(ActionEvent e) {
-						direction = 180;						
+						direction = 180;
 					}
-					
+
 				});
-				
+
 				JRadioButton eastButton = new JRadioButton("E");
 				eastButton.addActionListener(new ActionListener() {
 
 					@Override
 					public void actionPerformed(ActionEvent e) {
 						direction = 90;
-						
+
 					}
-					
+
 				});
-				
+
 				JRadioButton westButton = new JRadioButton("W");
 				westButton.addActionListener(new ActionListener() {
 
 					@Override
 					public void actionPerformed(ActionEvent arg0) {
 						direction = 270;
-						
+
 					}
-					
+
 				});
-				
+
 				ButtonGroup buttonGroup = new ButtonGroup();
 				buttonGroup.add(northButton);
 				buttonGroup.add(southButton);
 				buttonGroup.add(eastButton);
 				buttonGroup.add(westButton);
-				
+
 				okButton.addActionListener(new ActionListener() {
 
 					@Override
 					public void actionPerformed(ActionEvent e) {
 						String xCoordinate = xTextField.getText();
 						String yCoordinate = yTextField.getText();
-						
-						if((xCoordinate.length()>0) && (yCoordinate.length()>0) && (direction != -1)) {
+
+						if ((xCoordinate.length() > 0) && (yCoordinate.length() > 0) && (direction != -1)) {
 							gridPanel.addRobot(Integer.parseInt(xCoordinate), Integer.parseInt(yCoordinate), direction);
 							direction = -1;
 							addRobotFrame.setVisible(false);
 						}
 					}
-					
+
 				});
-				
+
 				addRobotCoordinatePanel.setLayout(new BoxLayout(addRobotCoordinatePanel, BoxLayout.X_AXIS));
 				addRobotCoordinatePanel.add(xLabel);
 				addRobotCoordinatePanel.add(xTextField);
@@ -234,19 +254,19 @@ public class PCGUI extends JFrame implements Runnable {
 				addRobotDirectionPanel.add(eastButton);
 				addRobotDirectionPanel.add(southButton);
 				addRobotDirectionPanel.add(westButton);
-				addRobotPanel.add(addRobotCoordinatePanel,BorderLayout.NORTH);
+				addRobotPanel.add(addRobotCoordinatePanel, BorderLayout.NORTH);
 				addRobotPanel.add(addRobotDirectionPanel, BorderLayout.CENTER);
 				addRobotPanel.add(okButton, BorderLayout.SOUTH);
 				addRobotFrame.add(addRobotPanel);
-				addRobotFrame.setPreferredSize(new Dimension(250,120));
+				addRobotFrame.setPreferredSize(new Dimension(250, 120));
 				addRobotFrame.pack();
 				addRobotFrame.setVisible(true);
-				
+
 			}
-			
+
 		});
 		toolsMenu.add(addRobotMenuItem);
-		
+
 		this.setJMenuBar(menuBar);
 
 		updateUI();
@@ -258,6 +278,7 @@ public class PCGUI extends JFrame implements Runnable {
 	public void updateUI() {
 		activeJobsInnerPanel.removeAll();
 		inactiveJobsInnerPanel.removeAll();
+		
 		for (String jobID : jobDataStore.getJobTable().keySet()) {
 			Job j = jobDataStore.getJobTable().get(jobID);
 			float percentageComplete = j.getPercentageComplete();
@@ -269,6 +290,10 @@ public class PCGUI extends JFrame implements Runnable {
 			}
 
 		}
+//		
+//		robotDetailsInnerPanel.removeAll();
+//		robotDetailsInnerPanel = new RobotPanel(server);
+//		
 		revalidate();
 	}
 
@@ -295,8 +320,6 @@ class JobPanel extends JPanel {
 	private JLabel jobLabel;
 
 	private JButton cancelButton;
-
-	private JobTable jobDataStore;
 
 	private JLabel percentageCompleteLabel;
 
@@ -338,6 +361,29 @@ class JobPanel extends JPanel {
 
 }
 
+class RobotPanel extends JPanel{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3025088648394354280L;
+	private List<Robot> robotList;
+	
+	public RobotPanel(Server server) {
+		robotList = server.getConnectedRobots();
+		for(Robot r : robotList) {
+			JPanel robotInnerPanel = new JPanel();
+			JLabel robotNameLabel = new JLabel(r.getName());
+			JLabel robotPositionLabel = new JLabel("Position: " + "(" + r.getX() + "," + r.getY() + ")");
+			JLabel robotWeightLabel = new JLabel("Weight: " + r.getCurrentWeight() + "/" + r.getMaxWeight());
+			robotInnerPanel.add(robotNameLabel, new BoxLayout(robotInnerPanel, BoxLayout.Y_AXIS));
+			robotInnerPanel.add(robotPositionLabel, new BoxLayout(robotInnerPanel, BoxLayout.X_AXIS));
+			robotInnerPanel.add(robotWeightLabel, new BoxLayout(robotInnerPanel, BoxLayout.X_AXIS));
+			add(robotInnerPanel);
+		}
+	}
+}
+
 class GridPanel extends JPanel implements Runnable {
 
 	/**
@@ -348,29 +394,34 @@ class GridPanel extends JPanel implements Runnable {
 	private MapBasedSimulation sim;
 	private ArrayList<MobileRobotWrapper<MovableRobot>> wrapperList;
 	private List<Robot> robotList;
-	private HashMap<String, GridPilot> robotTable;
-	
+	private ConcurrentHashMap<String, GridPilot> robotTable;
+	GridPositionDistribution dist;
+	GridPositionDistributionVisualisation mapVis;
+
 	private Server server;
 
 	public GridPanel(Server server) {
 		gridMap = MapUtils.createRealWarehouse();
 		sim = new MapBasedSimulation(gridMap);
 		wrapperList = new ArrayList<MobileRobotWrapper<MovableRobot>>();
-		this.robotTable = new HashMap<>();
+		this.robotTable = new ConcurrentHashMap<>();
 		this.robotList = server.getConnectedRobots();
-		for(Robot r : robotList) {
+		for (Robot r : robotList) {
 			robotTable.put(r.getName(), addRobot(r.getCurrentX(), r.getCurrentY(), r.getCurrentDirection()));
-			//r.getCurrentWeight();
-			//r.getMaxWeight();
+			// r.getCurrentWeight();
+			// r.getMaxWeight();
 		}
-		
+
 	}
-	
+
 	public GridPilot addRobot(int x, int y, int direction) {
-		
+		return addRobot(x, y, direction, true);
+	}
+
+	public GridPilot addRobot(int x, int y, int direction, boolean updateScreen) {
 		GridPose gridStart;
-		
-		switch(direction) {
+
+		switch (direction) {
 		case 0:
 			gridStart = new GridPose(x, y, Heading.PLUS_Y);
 			break;
@@ -384,54 +435,96 @@ class GridPanel extends JPanel implements Runnable {
 			gridStart = new GridPose(x, y, Heading.MINUS_X);
 			break;
 		default:
-				gridStart = new GridPose(x, y, Heading.PLUS_Y);
+			gridStart = new GridPose(x, y, Heading.PLUS_Y);
 		}
-		
+
 		MobileRobotWrapper<MovableRobot> wrapper = sim.addRobot(SimulatedRobots.makeConfiguration(false, true),
 				gridMap.toPose(gridStart));
 		
-		wrapperList.add(wrapper);
-		
-		GridPositionDistribution dist = new GridPositionDistribution(gridMap);
-		GridPositionDistributionVisualisation mapVis = new GridPositionDistributionVisualisation(dist, gridMap);
-		MapVisualisationComponent.populateVisualisation(mapVis, sim);
-		removeAll();
-		add(mapVis);
-		
+		if (updateScreen) {
+			wrapperList.add(wrapper);
+
+			dist = new GridPositionDistribution(gridMap);
+			mapVis = new GridPositionDistributionVisualisation(dist, gridMap);
+			MapVisualisationComponent.populateVisualisation(mapVis, sim);
+			removeAll();
+			add(mapVis);
+		}
+
 		return new GridPilot(wrapper.getRobot().getPilot(), gridMap, gridStart);
 	}
 
 	@Override
 	public void run() {
-		while(true) {
-			for(Robot r : robotList) {
-				if(!(robotTable.get(r.getName()).getGridPose()).equals(new GridPose(r.getCurrentX(), r.getCurrentY(), degToHeading(r.getCurrentDirection())))) {
-					//robotTable.get(r.getName()).
+
+		while (true) {
+			ConcurrentHashMap<String, GridPilot> newRobotTable = new ConcurrentHashMap<String, GridPilot>(robotTable);
+
+			List<Robot> searchList = new ArrayList<Robot>(robotList);
+			// create up-to-date robot table
+			for (Robot r : searchList) {
+				newRobotTable.put(r.getName(), addRobot(r.getCurrentX(), r.getCurrentY(), r.getCurrentDirection(), false));
+				// r.getCurrentWeight();
+				// r.getMaxWeight();
+			}
+
+			// checks and corrects differences
+			for (String key : robotTable.keySet()) {
+				GridPose aPose = robotTable.get(key).getGridPose();
+				GridPose bPose = newRobotTable.get(key).getGridPose();
+				if ((aPose.getX() != bPose.getX()) || (aPose.getY() != bPose.getY())) {
+					// move robots
+					int horizontal = bPose.getX() - aPose.getX();
+					int vertical = bPose.getY() - bPose.getY();
+					System.out.println(key);
+					int moves;
+
+					moves = vertical;
+
+					if (vertical > 0) {
+						for (int i = 0; i < moves; i++) {
+							move(robotTable.get(key));
+						}
+					} else {
+						robotTable.get(key).rotateNegative();
+						robotTable.get(key).rotateNegative();
+						for (int i = 0; i < Math.abs(moves); i++) {
+							move(robotTable.get(key));
+						}
+						robotTable.get(key).rotatePositive();
+						robotTable.get(key).rotatePositive();
+					}
+
+					moves = horizontal;
+					if (horizontal > 0) {
+						robotTable.get(key).rotateNegative();
+						for (int i = 0; i < moves; i++) {
+							move(robotTable.get(key));
+						}
+						robotTable.get(key).rotatePositive();
+					} else {
+						robotTable.get(key).rotatePositive();
+						for (int i = 0; i < Math.abs(moves); i++) {
+							move(robotTable.get(key));
+						}
+						robotTable.get(key).rotateNegative();
+					}
+
 				}
 			}
+
+			// Updates the current robot table
+			robotTable = newRobotTable;
+
 		}
 	}
-	
-	private Heading degToHeading(int angle) {
-		Heading direction;
-		
-		switch(angle) {
-		case 0:
-			direction = Heading.PLUS_Y;
-			break;
-		case 90:
-			direction = Heading.PLUS_X;
-			break;
-		case 180:
-			direction = Heading.MINUS_Y;
-			break;
-		case 270:
-			direction = Heading.MINUS_X;
-			break;
-		default:
-				direction = Heading.PLUS_Y;
-		}
-		
-		return direction;
+
+	private void move(GridPilot m_pilot) {
+
+		long delay = 250;
+
+		m_pilot.moveForward();
+
+		Delay.msDelay(delay);
 	}
 }
